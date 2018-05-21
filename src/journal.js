fe1b4f290a3a2dd39f863699c271d940dbb53766
Justin Roller and Noah Medoff <noah@>
2018-05-21 16:43:31 -0700

    start vowel counter

--- src/journal.js
+++ src/journal.js
@@ -7,4 +7,17 @@ Entry.prototype.counter = function() {
   var userInputArray = this.inputArray;
   var inputLength = userInputArray.length;
   return inputLength;
-};
+}
+
+Entry.prototype.vorc = function() {
+  var userInputArray = this.inputArray;
+  var numberOfVowels = 0;
+
+  userInputArray.forEach(function(letter) {
+    if (letter=="a"||letter=="e"||letter=="i"||letter=="o"||letter=="u"||letter=="A"||letter=="E"||letter=="I"||letter=="O"||letter=="U") {
+      numberOfVowels += 1;
+      console.log(numberOfVowels);
+    }
+  });
+  return numberOfVowels;
+}
